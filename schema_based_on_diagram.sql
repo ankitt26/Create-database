478ef241c8c21ec2b97f5e6b248aab92893903e3
Ankit <ankit26kushwa@gmail.comm>
2023-07-25 20:34:30 +0530

    Add foreign key / Add invoices table

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -10,3 +10,15 @@ CREATE TABLE patients (
     name VARCHAR,
     date_of_birth DATE
 );
+
+ALTER TABLE medical_histories 
+ADD FOREIGN KEY(patient_id) REFERENCES patients(id);
+
+
+CREATE TABLE invoices (
+    id SERIAL PRIMARY KEY,
+    total_amount DECIMAL,
+    generated_at TIMESTAMP,
+    payed_at TIMESTAMP,
+    medical_histories_id INT REFERENCES medical_histories(id)
+);
